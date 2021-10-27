20f34d541759a2b5c4e732ea28d4b53fdc59d3e1
charon961 <76212266+charon961@users.noreply.github.com>
2021-10-27 19:17:52 +0530

    Update project.py

--- project.py
+++ project.py
@@ -5,14 +5,14 @@ def screen_clear():
     _ = os.system('clear')
   else:
     _ = os.system('cls')
-sleep(5)
+sleep(1)
 screen_clear()
 
 from matplotlib import pyplot as plt
 from tabulate import tabulate
 import numpy as np
 
-class plant:
+class plant(object):
     #constructor
     def __init__(self, name, num,detail):
         self.name = name
@@ -98,8 +98,16 @@ class plant:
                 plt.legend()
                 plt.show()
         elif(self.num==6):
-            pass
+             pass
 
+
+    def change(self,num,s):
+            self.detail+=" "
+            self.detail+=s
+            return
+    def add(name,detail):
+         pass
+        
 
 
                 
@@ -109,7 +117,7 @@ class plant:
 
 
 
-obj1 = plant("Neem",1,"Details - \n Azadirachta indica, commonly known as neem, nimtree or Indian lilac, and in Nigeria called dogoyaro or dogonyaro, is a tree in the mahogany family Meliaceae.\n It is one of two species in the genus Azadirachta, and is native to the Indian subcontinent and most of the countries in Africa.\n Amount of Plantation - It is typically grown in tropical and semi-tropical regions.\n Neem trees also grow on islands in southern Iran. Its fruits and seeds are the source of neem oil.Uses - Neem leaves are dried in India and placed in cupboards to prevent insects eating the clothes, and also in tins where rice is stored. The flowers are also used in many Indian festivals like Ugadi.Effect on Environment - In 1995, the European Patent Office (EPO) granted a patent on an anti-fungal product derived from neem to the United States Department of Agriculture and W. R. Grace and Company. The Indian government challenged the patent when it was granted, claiming that the process for which the patent had been granted had been in use in India for more than 2,000 years.")
+obj1 = plant("Neem",1,"this is a origninal line")
 obj2 = plant("Rubber_tree",2,"Details - \n Rubber tree, (Hevea brasiliensis), South American tropical tree of the spurge family (Euphorbiaceae). It has soft wood; high, branching limbs; and a large area of bark. The milky liquid (latex) that oozes from any wound to the tree bark contains about 30 percent rubber, which can be coagulated and processed into solid products, such as tires.Amount of plantation:  Cultivated on plantations in the tropics and subtropics, especially in Southeast Asia and western AfricaUses: It is used in producing dipped goods, such as surgical glovesEffect: Over half of these plantations are in areas which are susceptible to insufficient water availability and soil erosion. Scientists have also linked rubber monoculture to reduction in water reserves, soil productivity and biodiversity in South-East Asia.")
 obj3 = plant("Tulsi",3,"Details - \n Ocimum tenuiflorum, commonly known as holy basil or tulsi, is an aromatic perennial plant in the family Lamiaceae. It is native to the Indian subcontinent and widespread as a cultivated plant throughout the Southeast Asian tropics. Amount of Plantation - The three main morphotypes cultivated in India and Nepal are Ram tulsi (the most common type, with broad bright green leaves that are slightly sweet), the less common purplish green-leaved (Krishna or Shyam tulsi) and the common wild vana tulsi. Uses - Tulsi (Sanskrit:-Surasa) has been used in Ayurveda and Siddha practices for its supposed treatment of diseases. For centuries, the dried leaves have been mixed with stored grains to repel insects.Effect on Environment - Tulsi is a sacred plant for Hindus. It is worshipped as the avatar of Lakshmi, and may be planted in courtyards of Hindu houses or Hanuman temples. The ritual lighting of lamps each evening during Kartik includes the worship of the tulsi plant. Vaishnavas followers of Vishnu are known as those who bear the tulsi around the neck.")
 obj4 = plant("Money_Plant",4,"Details - \n Epipremnum aureum is a species in the arum family Araceae, native to Mo orea in the Society Islands of French Polynesia. The species is a popular houseplant in temperate regions but has also become naturalised in tropical and sub-tropical forests worldwide, including northern South Africa, Australia, Southeast Asia, South Asia, the Pacific Islands and the West Indies, where it has caused severe ecological damage in some cases.Amount of Plantation - In 2021 an old vine with naturally occurring fruit was unofficially reported to have been found in South Florida which had infructescenses covered by green hexagonal scales approximately 3 mm (1/8 in) thick, which scales eventually peeled away to reveal an orange kernel fruit eaten by local wildlife (probably squirrels) and having an odor similar to a ripe cantaloupe melon.Uses - The plant can remove indoor pollutants such as formaldehyde, trichloroethene, toluene, xylene, and benzene in controlled circumstances (e.g. a sealed room). A study found that this effect declined as the molecular weight of the polluting substance increased. Effect on Environment - The plant is listed as toxic to cats and dogs by the ASPCA, because of the presence of insoluble raphides. Care should be taken to ensure the plant is not consumed by pets. Symptoms may include oral irritation, vomiting, and difficulty in swallowing.")
@@ -118,18 +126,24 @@ obj6 = plant("Peepal",6,"Details - \n Ficus religiosa or sacred fig is a species
 obj7 = plant("Maple",7,"Details - \n Acer is a genus of trees and shrubs commonly known as maples. The genus is placed in the family Sapindaceae.[1] There are approximately 132 species, most of which are native to Asia with a number also appearing in Europe, northern Africa, and North America. The maples usually have easily recognizable palmate leaves (Acer negundo is an exception) and distinctive winged fruits. The closest relatives of the maples are the horse chestnuts. Maple syrup is made from the sap of some maple species.Amount of Plantation: Many maples have bright autumn foliage, and many countries have leaf-watching traditions. The sugar maple (Acer saccharum) is the primary contributor to fall foliage season in North America.In Japan, the custom of viewing the changing colour of maples in the autumn is called momijigari. Nikko and Kyoto are particularly favoured destinations for this activity. In Korea, the same viewing activity is called danpung-nori and the Seoraksan and Naejang-san mountains are among the best-known destinations.Uses: Maple wood is commonly used in high-end furniture, flooring, cabinetry, and kitchen accessories. Because of its durability and strength, maple can be found used as flooring in bowling alleys and for bowling pins.Effect: the maple trees  in Québec capture the carbon produced by the equivalent of 290,000 cars in a year. That’s 9% of all cars in Québec.")
 obj8 = plant("Mango",8,"Details - \n A mango is an edible stone fruit produced by the tropical tree Mangifera indica which is believed to have originated from the region between northwestern Myanmar, Bangladesh, and northeastern India.[1] M. indica has been cultivated in South and Southeast Asia since ancient times resulting in two distinct types of modern mango cultivars: the Indian type and the Southeast Asian type.[2][3] Other species in the genus Mangifera also produce edible fruits that are also called mangoes, the majority of which are found in the Malesian ecoregionAmount of Plantation: Worldwide, there are several hundred cultivars of mango. Depending on the cultivar, mango fruit varies in size, shape, sweetness, skin color, and flesh color which may be pale yellow, gold, green, or orange.[5] The mango is the national fruit of India, Pakistan and the Philippines,[6][7] while the mango tree is the national tree of Bangladesh.[8]Uses: Mangos have been an important crop in India for millennia. Today, these colorful, sweet fruits are a mainstay of Indian cuisine and are popular throughout the world. The vitamins, minerals, and antioxidants in mangos can provide important health benefits. For example, vitamin K helps your blood clot effectively and helps prevent anemia. It also plays an important role in helping strengthen your bones.Mangos are also rich in vitamin C, which is important for forming blood vessels and healthy collagen, as well as helping you heal.Effects: The use of agrochemicals (including fertilizers) and transport of mangos together account for approximately 60% of the industry's greenhouse gas emissions. produced for each kg of mango fruit produced. The majority of these emissions (57%) come from the production and use of agrochemicals, notably fertilizers.")
 obj9 = plant("Mint",9,"Details - \n Mentha (also known as mint, from Greek μίνθα míntha,[2] Linear B mi-ta[3]) is a genus of plants in the family Lamiaceae (mint family).[4] The exact distinction between species is unclear; it is estimated that 13 to 24 species exist.[5][1] Hybridization occurs naturally where some species' ranges overlap. Many hybrids and cultivars are known.Amount of Plantation - The most common and popular mints for commercial cultivation are peppermint (Mentha × piperita), native spearmint (Mentha spicata), Scotch spearmint (Mentha x gracilis), and cornmint (Mentha arvensis);[13] also (more recently) apple mint (Mentha suaveolens). Mints are supposed to make good companion plants, repelling pesty insects and attracting beneficial ones. They are susceptible to whitefly and aphids.Uses - The leaf, fresh or dried, is the culinary source of mint. Fresh mint is usually preferred over dried mint when storage of the mint is not a problem. The leaves have a warm, fresh, aromatic, sweet flavor with a cool aftertaste, and are used in teas, beverages, jellies, syrups, candies, and ice creams. Menthol from mint essential oil (40–90%) is an ingredient of many cosmetics and some perfumes. Menthol and mint essential oil are also used in aromatherapy which may have clinical use to alleviate post-surgery nausea.Effects on Environment - Mentha pliocenica fossil seeds have been excavated in Pliocene deposits of Dvorets on the right bank of the Dnieper river between the cities of Rechitsa and Loyew, in south-eastern Belarus. The fossil seeds are similar to the seeds of Mentha aquatica and Mentha arvensis.")
-
-
+ls=[]
+new=9
 i=1  
 # ls=[["Neem","Rubber_tree","Tulsi"],["MoneyPlant","Aloe_Vera","Peepal"],["Maple","Mango","Mint"]]
 mydata = [["1", "Neem"],  ["2", "Rubber tree"], ["3", "Tulsi"],  ["4", "Money plant"],["5", "Aloe Vera"], ["6", "Peepal"],   ["7", "Mapple"], ["8", "Mango"],["9", "Mint"]]
-num=1;
+num=1
 while  i>0: 
  print("-----------------------------------------------------------------------------------------")
  head = ["S.no", "Name"]
   
   # display table
  print(tabulate(mydata, headers=head, tablefmt="grid"))
+ if new>9:
+     print("Newly added data: ")
+     print(" ")
+     for obj in ls:
+         print(" ",obj.num," ",obj.name," ",obj.detail)
+      
 #  for i in range(3):
 #     for j in range(3):
 #        print("   ",ls[i][j],end=" ")  
@@ -138,19 +152,19 @@ while  i>0:
 
     
  print("-----------------------------------------------------------------------------------------")
- user=int(input("---Enter a choice---\n\n1. About\n\n2. Plantation graphs\n\n3. Add information\n\n4. Remove a plant \n\n5. Add a plant\n\n0. Exit\n\n"))
+ user=int(input("---Enter a choice---\n\n1. About\n\n2. Plantation graphs\n\n3. Add information\n\n4. Add a plant \n\n5. Exit\n\n"))
  print("-----------------------------------------------------------------------------------------")
 
  
- if user==0:
+ if user==5:
      break
  elif user==1:
     plant_num=int(input("Which Plant : "))
     if plant_num==1:
-        sleep(5)
+        sleep(1)
         screen_clear()
         obj1.display(obj1)
-        sleep(45)
+        sleep(1)
         screen_clear()
     elif plant_num==2:
         sleep(5)
@@ -239,10 +253,79 @@ while  i>0:
         sleep(5)
         obj9.graph(9)
         screen_clear()
-      
+ elif user==3:
+     plant_num=int(input("Which Plant : "))
+     if plant_num==1:
+        sleep(1)
+        screen_clear()
+        s=input("add line: ")
+        obj1.change(plant_num,s)
+        sleep(1)
+        screen_clear()
+     if plant_num==2:
+        sleep(1)
+        screen_clear()
+        s=input("add line: ")
+        obj2.change(plant_num,s)
+        sleep(1)
+        screen_clear()
+     if plant_num==3:
+        sleep(1)
+        screen_clear()
+        s=input("add line: ")
+        obj3.change(plant_num,s)
+        sleep(1)
+        screen_clear()
+     if plant_num==4:
+        sleep(1)
+        screen_clear()
+        s=input("add line: ")
+        obj4.change(plant_num,s)
+        sleep(1)
+        screen_clear()
+     if plant_num==5:
+        sleep(1)
+        screen_clear()
+        s=input("add line: ")
+        obj5.change(plant_num,s)
+        sleep(1)
+        screen_clear()
+     if plant_num==6:
+        sleep(1)
+        screen_clear()
+        s=input("add line: ")
+        obj6.change(plant_num,s)
+        sleep(1)
+        screen_clear()
+     if plant_num==7:
+        sleep(1)
+        screen_clear()
+        s=input("add line: ")
+        obj7.change(plant_num,s)
+        sleep(1)
+        screen_clear()
+     if plant_num==8:
+        sleep(1)
+        screen_clear()
+        s=input("add line: ")
+        obj8.change(plant_num,s)
+        sleep(1)
+        screen_clear()
+     if plant_num==9:
+        sleep(1)
+        screen_clear()
+        s=input("add line: ")
+        obj9.change(plant_num,s)
+        sleep(1)
+        screen_clear()
+         
+ elif user==4:
+    plant_nam=input("Name of the plant : ")
+    plant_detail=input("About plant : ")
+    new+=1
+    new_obj=plant(plant_nam,new,plant_detail)
+    ls.append(new_obj)
+    sleep(1)
+    screen_clear()
+         
            
-
-    
-   
-
-   
